c261e50110b2740f21dee37bf3afded1f3b341fd
ifiamawood <821371350@qq.com>
2020-04-03 14:56:27 +0800

    11

--- 4.3.4/4.3.4.cpp
+++ 4.3.4/4.3.4.cpp
@@ -1,11 +1,28 @@
-// 4.3.4.cpp : 定义控制台应用程序的入口点。
-//
 
-#include "stdafx.h"
+#include"stdafx.h"
+#include <iostream>
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using std::string;
 
 
 int main()
 {
-    return 0;
-}
+	cv::Mat dstMat;
+	cv::Mat srcMat = cv::imread("D:\\lena.jpg", 1);
+	if (srcMat.empty()) return -1;
+	float angle = -10.0, scale = 1;
+	cv::Point2f center(srcMat.cols*0.5, srcMat.rows*0.5);
+	cv::Mat rot = cv::getRotationMatrix2D(center, angle, scale);
 
+	cv::Rect bbox = cv::RotatedRect(center, srcMat.size(), angle).boundingRect();
+
+	rot.at<double>(0, 2) += bbox.width*0.5 - center.x;
+	rot.at<double>(1, 2) += bbox.height*0.5 - center.y;
+
+	cv::warpAffine(srcMat, dstMat, rot, bbox.size());
+
+	cv::imshow("src", srcMat);
+	cv::imshow("dst", dstMat);
+	cv::waitKey(0);
+}
